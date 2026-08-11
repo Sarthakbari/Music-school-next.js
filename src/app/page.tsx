557c522da80a1410017c837919727c905e6806d4
Your Name <your.email@example.com>
2026-08-11 17:17:44 +0530

    config done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import { main } from "framer-motion/client";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-    <h1 className="text-center text-3xl text-blue-500">Hello World</h1>
+    <main className="min-h-screen bg-black/96 antialiased bg-grid-white/[0.02]">
+      <HeroSection />
     </main>
-  ); 
+  );
 }
