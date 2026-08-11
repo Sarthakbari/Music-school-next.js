b920d11280800e9e6f8db609e9b33ac2ca06a6ea
Your Name <your.email@example.com>
2026-08-11 16:34:36 +0530

    config done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} ${inter.className}`}>
-        <Navbar />
+
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
+
         {children}
+
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { main } from "framer-motion/client";
+
 export default function Home() {
   return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <h1 className="text-center text-3xl text-blue-500">Hello World</h1>
-  );
+    </main>
+  ); 
 }
